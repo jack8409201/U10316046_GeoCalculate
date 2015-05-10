4b1f4dd7bd40c8c05e8683ad29f4ecc98a62abbf
jack8409201 <jack8409201@gmail.com>
2015-05-10 23:30:57 +0800

    Modify GeoCalculate class
    將正方形,長方形的面板做出

--- GeoCalculate.java
+++ GeoCalculate.java
@@ -4,15 +4,36 @@ import java.awt.event.*;
 
 public class GeoCalculate extends JFrame implements ActionListener{
 	private JPanel circlePanel = new JPanel();
+	private JPanel squarePanel = new JPanel();
+	private JPanel rectanglePanel = new JPanel();
 	private JLabel jlbCircle = new JLabel("Circle : ");
+	private JLabel jlbSquare = new JLabel("Square : ");
+	private JLabel jlbRectangle = new JLabel("Rectangle : ");
 	private JPanel p1 = new JPanel();
 	private JPanel p2 = new JPanel();
 	private JPanel p3 = new JPanel();
+	private JPanel p4 = new JPanel();
+	private JPanel p5 = new JPanel();
+	private JPanel p6 = new JPanel();
+	private JPanel p7 = new JPanel();
+	private JPanel p8 = new JPanel();
+	private JPanel p9 = new JPanel();
 	private JTextField jtfRadius = new JTextField();
+	private JTextField jtfSide = new JTextField();
+	private JTextField jtfLength = new JTextField();
+	private JTextField jtfWidth = new JTextField();
 	private JTextArea jtaArea1 = new JTextArea();
+	private JTextArea jtaArea2 = new JTextArea();
+	private JTextArea jtaArea3 = new JTextArea();
 	private JTextArea jtaPerimeter1 = new JTextArea();
+	private JTextArea jtaPerimeter2 = new JTextArea();
+	private JTextArea jtaPerimeter3 = new JTextArea();
 	private JButton jbtCalculate1 = new JButton("Calculate");
+	private JButton jbtCalculate2 = new JButton("Calculate");
+	private JButton jbtCalculate3 = new JButton("Calculate");
 	private JButton jbtClean1 = new JButton("Clean");
+	private JButton jbtClean2 = new JButton("Clean");
+	private JButton jbtClean3 = new JButton("Clean");
 	
 	public GeoCalculate(){
 		setLayout(new BorderLayout(10,10));
@@ -40,13 +61,63 @@ public class GeoCalculate extends JFrame implements ActionListener{
 		add(circlePanel,BorderLayout.NORTH);
 		circlePanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 	
+		squarePanel.setLayout(new BorderLayout(10,10));
+		p4.setLayout(new FlowLayout(FlowLayout.LEFT));
+		p4.add(jlbSquare);
+		jlbSquare.setFont(new Font("TimesRoman",Font.BOLD,15));
+		squarePanel.add(p4,BorderLayout.NORTH);
+		p5.setLayout(new GridLayout(3,2,5,5));
+		p5.add(new JLabel("Side : "));
+		p5.add(jtfSide);
+		p5.add(new JLabel("The area is : "));
+		p5.add(jtaArea2);
+		jtaArea2.setEditable(false);
+		jtaArea2.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY)); 
+		p5.add(new JLabel("The perimeter is : "));
+		p5.add(jtaPerimeter2);
+		jtaPerimeter2.setEditable(false);
+		jtaPerimeter2.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		squarePanel.add(p5,BorderLayout.CENTER);
+		p6.setLayout(new GridLayout(2,1,10,10));
+		p6.add(jbtCalculate2);
+		p6.add(jbtClean2);
+		squarePanel.add(p6,BorderLayout.EAST);
+		add(squarePanel,BorderLayout.CENTER);
+		squarePanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		
+		rectanglePanel.setLayout(new BorderLayout(10,10));
+		p7.setLayout(new FlowLayout(FlowLayout.LEFT));
+		p7.add(jlbRectangle);
+		jlbRectangle.setFont(new Font("TimesRoman",Font.BOLD,15));
+		rectanglePanel.add(p7,BorderLayout.NORTH);
+		p8.setLayout(new GridLayout(4,2,5,5));
+		p8.add(new JLabel("Length : "));
+		p8.add(jtfLength);
+		p8.add(new JLabel("Width : "));
+		p8.add(jtfWidth);
+		p8.add(new JLabel("The area is : "));
+		p8.add(jtaArea3);
+		jtaArea3.setEditable(false);
+		jtaArea3.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY)); 
+		p8.add(new JLabel("The perimeter is : "));
+		p8.add(jtaPerimeter3);
+		jtaPerimeter3.setEditable(false);
+		jtaPerimeter3.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		rectanglePanel.add(p8,BorderLayout.CENTER);
+		p9.setLayout(new GridLayout(2,1,30,30));
+		p9.add(jbtCalculate3);
+		p9.add(jbtClean3);
+		rectanglePanel.add(p9,BorderLayout.EAST);
+		add(rectanglePanel,BorderLayout.SOUTH);
+		rectanglePanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+	
 	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new GeoCalculate();
 		frame.setTitle("U10316046_GeoCalculate");
 		frame.pack();
-		frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null); // Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
